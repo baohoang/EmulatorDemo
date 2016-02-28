d78ce871cbbcf4611cccd55798504dbf0c572d88
hungnd1 <hungnd1@vivas.vn>
2016-02-28 14:39:13 +0700

    update

--- emulator/src/main/java/global/vn/hn/emulator/GlobalKeyListenerExample.java
+++ emulator/src/main/java/global/vn/hn/emulator/GlobalKeyListenerExample.java
@@ -1,5 +1,14 @@
 package global.vn.hn.emulator;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
@@ -7,8 +16,20 @@ import org.jnativehook.keyboard.NativeKeyListener;
 
 public class GlobalKeyListenerExample implements NativeKeyListener {
 	public void nativeKeyPressed(NativeKeyEvent e) {
-		System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-
+//		System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+		try {						
+			DateFormat date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Date d = new Date();
+			FileWriter fw = new FileWriter("input.txt",true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(date.format(d)+"-"+NativeKeyEvent.getKeyText(e.getKeyCode()).toString()+"_");	
+			bw.close();
+			fw.close();
+			
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}		
 		if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
 			try {
 				GlobalScreen.unregisterNativeHook();
@@ -20,16 +41,21 @@ public class GlobalKeyListenerExample implements NativeKeyListener {
 	}
 
 	public void nativeKeyReleased(NativeKeyEvent e) {
-		System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+//		System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
 	}
 
 	public void nativeKeyTyped(NativeKeyEvent e) {
-		System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+//		System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
 	}
 
 	public static void main(String[] args) {
 		try {
-			GlobalScreen.registerNativeHook();
+		File file = new File("input.txt");
+			if(file.exists()){
+				file.delete();
+			}
+			System.out.println(1);
+			GlobalScreen.registerNativeHook();			
 		} catch (NativeHookException ex) {
 			System.err.println("There was a problem registering the native hook.");
 			System.err.println(ex.getMessage());
---
+++ emulator/src/main/java/global/vn/hn/emulator/Test.java
@@ -0,0 +1,10 @@
+package global.vn.hn.emulator;
+
+public class Test implements Runnable{
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
